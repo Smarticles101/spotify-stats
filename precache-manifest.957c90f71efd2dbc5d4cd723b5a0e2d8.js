fa91ff5552da4bede5b25bc60a34cf61844296b6
Logan Stucki <l@logans.me>
2020-02-22 15:18:22 -0700

    Updates

---
+++ precache-manifest.957c90f71efd2dbc5d4cd723b5a0e2d8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "77b7706d4addd8227eb28cb65f9c9e5d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3490f12dfea61bc0e9d9",
+    "url": "/static/css/main.75c297d6.chunk.css"
+  },
+  {
+    "revision": "02ae7ff744991ae7abe5",
+    "url": "/static/js/2.c28f7b00.chunk.js"
+  },
+  {
+    "revision": "3490f12dfea61bc0e9d9",
+    "url": "/static/js/main.4e2d2c35.chunk.js"
+  },
+  {
+    "revision": "d812968336e16f5a4e8d",
+    "url": "/static/js/runtime-main.6665b461.js"
+  }
+]);
